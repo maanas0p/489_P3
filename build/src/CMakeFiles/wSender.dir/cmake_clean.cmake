89d460105250254bec2be3f6d8699ebd7d02e885
maanasp <maanasp@umich.edu>
2025-11-04 22:46:40 -0500

    fixedf errorS

---
+++ build/src/CMakeFiles/wSender.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/wSender"
+  "../bin/wSender.pdb"
+  "CMakeFiles/wSender.dir/wSender/wSender.cpp.o"
+  "CMakeFiles/wSender.dir/wSender/wSender.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wSender.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
